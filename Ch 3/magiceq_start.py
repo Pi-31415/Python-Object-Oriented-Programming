b33341588acae3ede2e8ddedc5ba4e0cf4f6836b
Pi <paingthetko.pi@gmail.com>
2022-01-27 00:32:03 +0400

    .

--- Ch 3/magiceq_start.py
+++ Ch 3/magiceq_start.py
@@ -11,6 +11,21 @@ class Book:
 
     # TODO: the __eq__ method checks for equality between two objects
 
+    def __eq__(self, value):
+        if(not isinstance(value, Book)):
+            raise ValueError("Can't compare a book to a non-book.")
+        return(self.title == value.title and self.author == value.author and self.price == value.price)
+
+    def __ge__(self, value):
+        if(not isinstance(value, Book)):
+            raise ValueError("Can't compare a book to a non-book.")
+        return(self.price >= value.price)
+
+    def __lt__(self, value):
+        if(not isinstance(value, Book)):
+            raise ValueError("Can't compare a book to a non-book.")
+        return(self.price <= value.price)
+
     # TODO: the __ge__ establishes >= relationship with another obj
 
     # TODO: the __lt__ establishes < relationship with another obj
@@ -23,8 +38,17 @@ b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
 
+# Python does not do attribute by attribute comparison on objects
+
+print(b1 == b3)  # Returns false, if __eq__ is not overwritten
+print(b1 == b2)
+
 
 # TODO: Check for greater and lesser value
-
+print(b1 >= b1)
+print(b2 <= b3)
 
 # TODO: Now we can sort them too
+books = [b1, b2, b3, b4]
+books.sort()
+print([book.title for book in books])
