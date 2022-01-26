45d8ac71db56046e149d910626a7061fca718274
Pi <paingthetko.pi@gmail.com>
2022-01-26 13:19:33 +0400

    Update typecheck_start.py

--- Ch 1/typecheck_start.py
+++ Ch 1/typecheck_start.py
@@ -1,7 +1,6 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Checking class types and instances
 
-
 class Book:
     def __init__(self, title):
         self.title = title
@@ -20,8 +19,20 @@ n2 = Newspaper("The New York Times")
 
 # TODO: use type() to inspect the object type
 
+print(type(b1))
+print(type(n1))
 
 # TODO: compare two types together
 
 
+print(type(b1) == type(n2))
+print(type(b1) == type(b2))
+
 # TODO: use isinstance to compare a specific instance to a known type
+
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(b1, Newspaper))
+
+# In Python, everything is a subclass of the object class
+print(isinstance(b1, object))
